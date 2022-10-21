8420d53e3e46aefca9b32a9174875ec5df671c92
razasayyed29 <115571848+razasayyed29@users.noreply.github.com>
2022-10-21 06:26:53 -0700

    loop

---
+++ area.py
@@ -0,0 +1,5 @@
+c=float(input("enter circumference of a circle"))
+r=float("enter radius")
+r=c/3.14*2
+area=3.14*r*r
+print(area)
---
+++ si.py
@@ -0,0 +1,5 @@
+p=float(input("enter principle amt"))
+r=float(input("enter rate"))
+t=float(input("enter years"))  
+si=p*r*t/100
+print (si)
